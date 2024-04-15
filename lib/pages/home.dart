98c48563762e0dc523608142d0e370fd75267427
Florian Nguyen Van <floriannguyenvan@hotmail.com>
2024-04-15 19:17:39 +0200

    display time

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,32 +2,70 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  final Key? key;
+
+  const Home({this.key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+    try {
+      data = ModalRoute.of(context)!.settings.arguments as Map;
+      print(data);
+    }
+    catch (e) {
+      data = data;
+    }
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: () {
-              Navigator.pushNamed(context, '/location');
-            },
-              child: Row(children: [
-                Text("Edit Location"),
-                Icon(Icons.edit_location),
-              ],) ,
-              
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.edit_location),
+                    Text("Edit Location"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Accessing the "location" property safely using null-aware operator
+                  Text(
+                    data["location"] ?? "Unknown Location",
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                data["time"] ?? "Unknown Time",
+                style: TextStyle(
+                  fontSize: 66.0
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   late String location; //location name for the UI
@@ -12,19 +13,26 @@ class WorldTime {
 
   Future<void> getTime() async {
 
-    Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
-    Map data = jsonDecode(response.body);
+    try {
+      Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+      Map data = jsonDecode(response.body);
 
-    //get properties from data
-    String datetime = data["datetime"];
-    String offset = data["utc_offset"].substring(1,3);
+      //get properties from data
+      String datetime = data["datetime"];
+      String offset = data["utc_offset"].substring(1,3);
 
-    //create a datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      //create a datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    //set the time property
-    time = now.toString();
+      //set the time property
+      time = DateFormat.jm().format(now);
+    } catch(e) {
+      print("caught error $e");
+      time = "could not get time data";
+    }
+
+
   }
 
 }
