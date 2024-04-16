f40e11bf97414c3b849f320b942d261b17ddcb7d
Florian Nguyen Van <floriannguyenvan@hotmail.com>
2024-04-16 15:50:44 +0200

    background picture

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,57 +12,82 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Map data = {};
+  String bgImage = "night.png";
+  Color? bgColor = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
     try {
       data = ModalRoute.of(context)!.settings.arguments as Map;
       print(data);
+
+      // set background
+      bgImage = data["isDaytime"] ?  "day.png" : "night.png";
+      bgColor = data["isDaytime"] ? Colors.blue: Colors.indigo[700];
     }
     catch (e) {
       data = data;
     }
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                child: Row(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/$bgImage"),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.edit_location,
+                        color: Colors.grey[300] ,
+                      ),
+                      Text(
+                          "Edit Location",
+                        style: TextStyle(
+                            color: Colors.grey[300]
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.edit_location),
-                    Text("Edit Location"),
+                    // Accessing the "location" property safely using null-aware operator
+                    Text(
+                      data["location"] ?? "Unknown Location",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    )
                   ],
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Accessing the "location" property safely using null-aware operator
-                  Text(
-                    data["location"] ?? "Unknown Location",
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data["time"] ?? "Unknown Time",
-                style: TextStyle(
-                  fontSize: 66.0
-                ),
-              )
-            ],
+                SizedBox(height: 20.0),
+                Text(
+                  data["time"] ?? "Unknown Time",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 66.0
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
